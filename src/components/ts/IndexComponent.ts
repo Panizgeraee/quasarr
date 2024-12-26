b33125254a0923d07b4c5e4e98abf247dd8f09ac
unknown <you@example.com>
2024-12-26 17:51:52 +0330

    Initial commit of Quasar project

---
+++ src/components/ts/IndexComponent.ts
@@ -0,0 +1,40 @@
+import { ref } from 'vue';
+
+const posts = ref([
+  {
+    id: 0,
+    image: 'images/mountains.jpg',
+    title: 'Fake Post',
+    username: 'Panizgeraee',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    upVoteCount: 10,
+  },
+  {
+    id: 0,
+    image: 'images/mountains.jpg',
+    title: 'Fake Post',
+    username: 'Panizgeraee',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    upVoteCount: 10,
+  },
+  {
+    id: 0,
+    image: 'images/mountains.jpg',
+    title: 'Fake Post',
+    username: 'Panizgeraee',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    upVoteCount: 10,
+  },
+  {
+    id: 0,
+    image: 'images/mountains.jpg',
+    title: 'Fake Post',
+    username: 'Panizgeraee',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    upVoteCount: 10,
+  },
+]);
+
+export {posts}
+
+
---
+++ src/components/ts/MyPostComponent.ts
@@ -0,0 +1,33 @@
+import { ref } from 'vue';
+
+const columns: any[] = [
+  { name: 'id', align: 'left', label: 'ID', field: 'id', sortable: true },
+  { name: 'title', align: 'center', label: 'Title', field: 'title', sortable: true },
+  { name: 'description', align: 'center', label: 'Description', field: 'description', sortable: true,format: (val:string) => `${val.slice(0,40)} ...`, },
+]
+
+const rows = ref([
+  {
+    id: 1,
+    image: 'images/mountains.jpg',
+    title: 'fake',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    id: 2,
+    image: 'images/mountains.jpg',
+    title: 'fake',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+
+]);
+
+const pagination = ref({
+  sortBy: 'desc',
+  descending: false,
+  page: 1,
+  rowsPerPage: 5,
+  rowsNumber: 100
+})
+
+export {columns, rows, pagination}
